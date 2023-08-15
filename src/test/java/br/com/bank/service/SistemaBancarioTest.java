982032893fc4c31a23eac23032597957f39fc335
Vinicius Brenner <vinicius.brenner@grupopzm.com>
2023-08-15 15:42:48 -0300

    Teste Mock usando verificação de quantidade de chamadas na função

--- src/test/java/br/com/bank/service/SistemaBancarioTest.java
+++ src/test/java/br/com/bank/service/SistemaBancarioTest.java
@@ -11,14 +11,23 @@ import org.mockito.quality.Strictness;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
 class SistemaBancarioTest {
 
-//    @InjectMocks
-//    private SistemaBancario sistemaBancario;
-//    @Mock
-//    private Bacen bacen;
+    @InjectMocks
+    private SistemaBancario sistemaBancario;
+
+    @Mock
+    private Bacen bacen;
+
+	@Test
+	public void validar_metodo_cadastrarBanco() {
+        Banco itau = new Banco("Itau");
+        sistemaBancario.registrarBanco(itau);
+        verify(bacen, times(1)).cadastrarBanco(itau);
+	}
 
 }
